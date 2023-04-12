eeae0edfc0ccc562b4725d863fe68291c05b4d89
Reo Petersen <Reoapetersen@gmail.com>
2023-04-12 23:32:40 +0000

    update

--- src/js/layout.js
+++ src/js/layout.js
@@ -84,3 +84,7 @@ const Layout = () => {
 };
 
 export default injectContext(Layout);
+
+
+
+
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -21,16 +21,30 @@ export const Home = () => (
 
 	<div className="px-3 my-4 bg-light rounded-3 text-center">
         <div className="container-fluid py-3">
-        <h1 className="display-5 fw-bold my-3" id="title">Welcome to the ToDo List!</h1>
-        <p className="col-md-12 fs-4 mx-auto my-4">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione magni vel aliquid, natus laudantium a beatae adipisci ab quam soluta pariatur, blanditiis totam corrupti sint suscipit! Dolor nam perferendis voluptatibus.
-        </p>
-        <Link to="/todo">
-          <button className="btn btn-primary btn-lg float-center mt-5 mb-3" type="button">
-            Click Here Start Entering Into The ToDo List
+        <h1 className="display-5 fw-bold my-3" id="title">Welcome to the ToDo-List API!</h1>
+       
+        <div className="card mt-3">
+        <div className="card-header">Begin by entering a username </div>
+        <div className="card-body">
+          <div className="form-group">
+            <input type="text" className="form-control" id="post-title" placeholder="Enter a Username"/>
+          </div>
+          <button className="btn btn-sm btn-primary mx-1" onClick={()=>{}}>Send</button>
+          {/* <button className="btn btn-sm btn-warning mx-1" onClick={()=>{}}>Clear</button> */}
+
+          <div id="postResult"></div>
+        </div>
+      </div>
+       
+        <Link to="/todoAPI">
+          <button className="btn btn-primary btn-lg float-center mt-5 mb-3 p-1" type="button">
+            Click Here to go to the API Database.
           </button>
         </Link>
       </div>
+      
     </div>
 	  
+
+    
 );
